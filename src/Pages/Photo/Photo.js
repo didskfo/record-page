451ec4c10a4ae9fe7435792e8bbf0f5ff10a07d3
didskfo <didskfo2003@gmail.com>
2023-07-18 23:36:08 +0900

    feat. Photo page layout

---
+++ src/Pages/Photo/Photo.js
@@ -0,0 +1,35 @@
+import * as style from "./styles";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function Photo() {
+  const [writeDate, setWriteDate] = useState(new Date());
+  return (
+    <>
+      <style.Title>WRITE</style.Title>
+      <style.Wrap>
+        <style.Inform>
+          <DatePicker
+            selected={writeDate}
+            onChange={(date) => setWriteDate(date)}
+            dateFormat="yyyy / MM / dd"
+          />
+          <style.Weather className="weather">
+            <img src={process.env.PUBLIC_URL + "Images/sunny.svg"} />
+            <img src={process.env.PUBLIC_URL + "Images/foggy.svg"} />
+            <img src={process.env.PUBLIC_URL + "Images/air.svg"} />
+            <img src={process.env.PUBLIC_URL + "Images/thunderstorm.svg"} />
+            <img src={process.env.PUBLIC_URL + "Images/snow.svg"} />
+          </style.Weather>
+        </style.Inform>
+        <style.Input>
+          <img src={process.env.PUBLIC_URL + "Images/Picture.svg"} />
+          <style.Textarea type="textarea" placeholder="내용을 입력하세요." />
+        </style.Input>
+      </style.Wrap>
+    </>
+  );
+}
+
+export default Photo;
---
+++ src/Pages/Photo/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 128px;
+  margin: 30px 0 30px 0;
+`;
+export const Wrap = styled.div`
+  margin: 0 66px 20px 66px;
+`;
+export const Inform = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 60px;
+`;
+export const Weather = styled.div`
+  width: 270px;
+  display: flex;
+  justify-content: space-between;
+`;
+export const Input = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const Textarea = styled.input`
+  width: 541px;
+  height: 471px;
+`;
